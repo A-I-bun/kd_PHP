cf9dcd998ee160e4a1892b673909afe185c532aa
sh1tensh0ku <jesuisenpanne7@gmail.com>
2020-03-08 05:55:12 +0900

    ファボ修正/RT追加

--- index.php
+++ index.php
@@ -43,16 +43,24 @@ $page=min($page,$maxPage);
 
 $start=($page-1)*5;
 
+// 元のSQL('SELECT m.name,m.picture,p.* FROM members m,posts p WHERE m.id=p.member_id ORDER BY p.created DESC LIMIT ?,5'); 
 $posts=$db->prepare
-('SELECT m.name,m.picture,p.* FROM members m,posts p WHERE m.id=p.member_id ORDER BY p.created DESC LIMIT ?,5');
+('SELECT a.post_id,p.message,p.member_id,m.name AS m_name,rm.reacted_member_id,rm.name AS r_name,m.picture,min.minCreated,fc.favCnt,rc.rtCnt 
+FROM posts p
+LEFT JOIN members m ON p.member_id=m.id
+-- ↓"SELECT p.id,p.created from posts p UNION SELECT r.post_id,r.created FROM retweets r"のビュー
+LEFT JOIN allCreated a ON p.id=a.post_id
+-- ↓postidごとの投稿者本人が投稿した日付表示をRTにも適用させる用
+LEFT JOIN (SELECT a.post_id,MIN(a.created) AS minCreated FROM allCreated a GROUP BY a.post_id) AS min ON p.id=min.post_id
+-- ↓RT者の名前表示用
+LEFT JOIN (SELECT m.name,r.reacted_member_id,r.created FROM members m JOIN retweets r ON m.id=r.reacted_member_id) AS rm ON a.created=rm.created
+-- ↓ファボ数出力
+LEFT JOIN (SELECT post_id, COUNT(post_id) AS favCnt FROM favorites GROUP BY post_id) AS fc ON p.id=fc.post_id
+-- ↓RT数出力
+LEFT JOIN (SELECT post_id, COUNT(post_id) AS rtCnt FROM retweets GROUP BY post_id) AS rc ON p.id=rc.post_id
+ORDER BY a.created DESC LIMIT ?,5');
 $posts->bindParam(1,$start,PDO::PARAM_INT);
 $posts->execute();
-
-//ファボのカウント取得
-  $favCounts=$db->query
-  ('SELECT post_id, COUNT(post_id) AS favCnt FROM favorites GROUP BY post_id');
-  $favCnt=$favCounts->fetchall();
-  // var_dump($favCnt);
 
 //ファボ色分岐用
   $favColors=$db->prepare('SELECT post_id FROM favorites WHERE reacted_member_id=?');
@@ -60,8 +68,15 @@ $posts->execute();
     $_SESSION['id']
   ));
   $favColor=$favColors->fetchall();
-  var_dump($favColor);
+  // var_dump($favColor);
 
+//RT色分岐
+  $rtColors=$db->prepare('SELECT post_id FROM retweets WHERE reacted_member_id=?');
+  $rtColors->execute(array(
+    $_SESSION['id']
+  ));
+  $rtColor=$rtColors->fetchall();
+  
 //返信
 if(isset($_REQUEST['res'])) {
   $response=$db->prepare('SELECT m.name,m.picture,p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=? ORDER BY p.created DESC');
@@ -84,7 +99,7 @@ if(isset($_REQUEST['res'])) {
 
 <body>
 <div id="content">
-<div style="text-align:right"><a href="logout.php">ログアウト</a></div>
+<p class="logout"><a href="logout.php">ログアウト</a></p>
 <!-- 投稿画面 -->
 <form action="" method="post">
   <p><?php echo h($member['name']); ?>さん、メッセージをどうぞ</p>
@@ -95,6 +110,7 @@ if(isset($_REQUEST['res'])) {
   </div>
 </form>
 
+<div class="page_crearfix">
 <ul class="paging">
   <?php if($page>1): ?>
     <li><a href="index.php?page=<?php print($page-1); ?>">前のページへ</a></li>
@@ -108,19 +124,34 @@ if(isset($_REQUEST['res'])) {
     <li>次のページへ</li>
   <?php endif; ?>
 </ul>
+</div>
 
 <!-- 投稿内容反映部分 -->
-<?php foreach($posts as $post): ?>
+<?php
+foreach($posts as $post):
+  // print_r($post);
+?>
 <div class="msg">
 <!-- 画像＆投稿内容&リプ -->
-  <img src="member_picture/<?php echo h($post['picture']);?>" width="48" height="50" alt="<?php echo h($post['name']); ?>">
+<?php
+if($_SESSION['id']===$post['reacted_member_id']):
+?>
+  <p>リツイート済み</p>
+<?php else: ?>
+  <p>&#x21B9;<?php echo h($post['r_name']);?>  さんがリツイート</p>
+<?php endif; ?>
+
+  <img src="member_picture/<?php echo h($post['picture']);?>" width="48" height="50" alt="<?php echo h($post['m_name']); ?>">
   <p><?php echo makeLink(h($post['message'])); ?>
-  <span class="name">(<?php echo h($post['name']); ?>)</span></p>
+  <span class="name">(<?php echo h($post['m_name']); ?>)</span></p>
 
 <div class="re_del_flex">
-<!-- 返信の部分 -->
-  [<a class="reply" href="index.php?res=<?php echo h($post['id']); ?>">Re</a>]
-  <p class="day"><a href="view.php?id=<?php echo h($post['id']); ?>"><?php echo h($post['created']); ?></a></p>
+<!-- 返信&日付の部分 -->
+  [<a class="reply" href="index.php?res=<?php echo h($post['post_id']); ?>">Re</a>]
+  
+  <p class="day"><a href="view.php?id=<?php echo h($post['post_id']); ?>">
+  <?php echo h($post['minCreated']); ?></a></p>
+
   <?php
   if($post['reply_post_id'] > 0):
   ?>
@@ -131,46 +162,53 @@ if(isset($_REQUEST['res'])) {
   <?php
   if($_SESSION['id']==$post['member_id']):
   ?>
-    [<a class="delete" href="delete.php?id=<?php echo h($post['id']); ?>" style="color:#F33;">削除</a>]
+    [<a class="delete" href="delete.php?id=<?php echo h($post['post_id']); ?>">削除</a>]
   <?php endif; ?>
 </div> 
 </div>
-<!-- ファボ -->
-<!-- aタグ分岐・ -->
-<div class="favorite">  
+
+<div class="rt_fav">
+<div class="retweet">
   <?php
-  foreach($favColor as $fCol):
-  if($post['id']===$fCol['post_id']):
-  ?>
-  <?php
-  $fBranch=$fCol['post_id'];
-  var_dump($fBranch);
-  ?>
-  <?php
-  endif; 
-  endforeach;
-  ?>
-  <?php if($fBranch>0):
-    var_dump($fBranch>0); ?>
-    <a class ="f_yes" href="favorite.php?id=<?php echo h($post['id']); ?>">★
-  <?php
-    foreach($favCnt as $fCnt){
-      if($post['id']===$fCnt['post_id']){
-      echo h($fCnt['favCnt']);
-      }
-    }?>
-  </a>
-  <?php else: ?>
-  <a class ="f_no" href="favorite.php?id=<?php echo h($post['id']); ?>">☆
-  <?php
-  foreach($favCnt as $fCnt){
-    if($post['id']===$fCnt['post_id']){
-    echo h($fCnt['favCnt']);
+  $rtBranch=0;
+  foreach($rtColor as $rtCol){
+  if($post['post_id']===$rtCol['post_id']){
+    $rtBranch=$rtCol['post_id'];
     }
-  }?></a>
+  }
+  if($rtBranch>0):
+  ?>
+    <a class="r_yes" href="retweet.php?id=<?php echo h($post['post_id']); ?>">&#x21B9;
+    <?php echo h($post['rtCnt']) ?>
+    </a>
+  <?php else: ?>
+    <a class="r_no" href="retweet.php?id=<?php echo h($post['post_id']); ?>">&#x21B9;
+    <?php echo h($post['rtCnt']) ?>
+    </a>
   <?php endif; ?>
 </div>
 
+<!-- ファボ -->
+<!-- aタグ分岐・ -->
+<div class="favorite">  
+<?php
+  $fBranch=0;
+  foreach($favColor as $fCol){
+  if($post['post_id']===$fCol['post_id']){
+    $fBranch=$fCol['post_id'];
+  }
+}
+if($fBranch>0):
+?>
+  <a class ="f_yes" href="favorite.php?id=<?php echo h($post['post_id']); ?>">★
+  <?php echo h($post['favCnt']) ?>
+  </a>
+<?php else: ?>
+  <a class ="f_no" href="favorite.php?id=<?php echo h($post['post_id']); ?>">☆
+  <?php echo h($post['favCnt']) ?></a>
+<?php endif; ?>
+</div> <!--favoriteクラス-->
+</div><!--rt_fav-->
 </div>
 <?php endforeach; ?>
 
